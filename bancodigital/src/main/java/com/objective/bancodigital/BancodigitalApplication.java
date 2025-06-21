e724fa7edbcd54c854056065624f4f7f94af4fb7
Eduardo Malheiro <eduardohdvm@hotmail.com>
2025-06-21 11:42:25 -0300

    initial project setup for bank-api

---
+++ bancodigital/src/main/java/com/objective/bancodigital/BancodigitalApplication.java
@@ -0,0 +1,14 @@
+package com.objective.bancodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancodigitalApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BancodigitalApplication.class, args);
+	}
+
+}
