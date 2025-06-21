888fd59d161cfd22aa6f454327b84e28f6e1182d
Eduardo Malheiro <eduardohdvm@hotmail.com>
2025-06-21 19:40:36 -0300

    chore: remove unused JPAConfig class

--- bancodigital/src/main/java/com/objective/bancodigital/infrastructure/persistence/JPAConfig.java
+++
@@ -1,8 +0,0 @@
-package com.objective.bancodigital.infrastructure.persistence;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class JPAConfig {
-    // configurações específicas JPA se necessário
-}
