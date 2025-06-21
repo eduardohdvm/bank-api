ce0b7c73f8fd71d9750aba7f2bccb6a36e107005
Eduardo Malheiro <eduardohdvm@hotmail.com>
2025-06-21 19:19:44 -0300

    feat: return HTTP 404 when account is not found

--- bancodigital/src/main/java/com/objective/bancodigital/api/exceptionhandler/ApiExceptionHandler.java
+++ bancodigital/src/main/java/com/objective/bancodigital/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,16 +1,17 @@
 package com.objective.bancodigital.api.exceptionhandler;
 
+import com.objective.bancodigital.domain.exceptions.ContaNaoEncontradaException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ApiExceptionHandler {
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    @ExceptionHandler(ContaNaoEncontradaException.class)
+    public ResponseEntity<String> handleContaNaoEncontrada(ContaNaoEncontradaException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 }
 
